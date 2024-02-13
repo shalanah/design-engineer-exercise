9c1f57b64d658589ee0655e9b61160c4fa8923d0
shalanah <shalanahfaith@gmail.com>
2024-02-12 19:24:12 -0600

    mock in no matches - and outside matches

--- app/comps/StaffMembers.tsx
+++ app/comps/StaffMembers.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import people from "../../team.json";
-import { StaffMember } from "./StaffMember";
+import { StaffMember, StaffMemberProps } from "./StaffMember";
 import { Select } from "./Select";
 import {
   Cross2Icon,
@@ -35,10 +35,57 @@ const teamData = Object.fromEntries(
 const selectItems = [
   { text: "All", value: "all" },
   ...teams.map((v) => {
-    return { text: `${v} (${teamData[v].count})`, value: v };
+    return {
+      text: (
+        <div
+          className="d-flex justify-content-between align-items-center"
+          style={{ minWidth: 120 }}
+        >
+          <span>{v}</span>
+          <span
+            className="heading"
+            style={{
+              fontSize: ".6rem",
+              padding: "8px 3px",
+              lineHeight: 0,
+              borderRadius: 100,
+              minWidth: 22,
+              textAlign: "center",
+              background: teamData[v].color,
+              color: "#fff",
+            }}
+          >
+            {teamData[v].count}
+          </span>
+        </div>
+      ),
+      value: v,
+    };
   }),
 ];
 
+// Function to normalize strings by removing accents
+function normalizeString(str: string): string {
+  return str
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase();
+}
+const textSearch = (member: StaffMemberProps, cleanSearch: string) =>
+  (
+    [
+      "name",
+      "team",
+      "role",
+      "location",
+      "country",
+      "city",
+    ] as (keyof StaffMemberProps)[]
+  ).some((key) =>
+    normalizeString(member[key]).includes(normalizeString(cleanSearch))
+  );
+
+// TODO: Scroll to top on search or filter change
 export const StaffMembers = () => {
   const [selectedTeams, setSelectedTeams] = useState(teams); // maybe someday multiple departments can be selected
   const [isSearching, setIsSearching] = useState(false); // maybe someday multiple departments can be selected
@@ -51,11 +98,13 @@ export const StaffMembers = () => {
   const filteredPeople = people.data.filter(
     (member) =>
       selectedTeams.includes(member.team) &&
-      (!isSearching ||
-        !useSearch ||
-        member.name.toLowerCase().includes(search.toLowerCase()))
+      (!isSearching || !useSearch || textSearch(member, cleanSearch))
   );
   const count = filteredPeople.length;
+  const outsideSearch =
+    count === 0
+      ? people.data.filter((member) => textSearch(member, cleanSearch))
+      : [];
   return (
     <div style={{ margin: "30px auto", maxWidth: 500 }}>
       <div
@@ -80,45 +129,50 @@ export const StaffMembers = () => {
             />
           </label>
         )}
-        <Select
-          onValueChange={(value) => {
-            if (value === "all") setAllTeamsSelected();
-            else setSelectedTeams([value]);
-          }}
-          value={showAllTeams ? "all" : selectedTeams[0]}
-          button={
-            <button
-              className="d-flex align-items-center"
-              style={{ gap: 3, marginLeft: 5 }}
-            >
-              Departments <ChevronDownIcon style={{ marginTop: "-2" }} />
-            </button>
-          }
-          items={selectItems}
-        />
-        <span>
-          {showAllTeams ? null : (
-            <>
-              {selectedTeams.map((item) => (
-                <button
-                  onClick={setAllTeamsSelected}
-                  className="d-flex align-items-center"
-                  style={{
-                    gap: ".4em",
-                    fontSize: ".75rem",
-                    padding: ".25em .75em",
-                    border: "1px solid #ccc",
-                    borderRadius: "1em",
-                  }}
-                  key={item}
-                >
-                  {item}
-                  <Cross2Icon width={10} height={10} />
-                </button>
-              ))}
-            </>
-          )}
-        </span>
+        <div
+          className="d-flex align-items-center justify-content-center"
+          style={{ gap: 5 }}
+        >
+          <Select
+            onValueChange={(value) => {
+              if (value === "all") setAllTeamsSelected();
+              else setSelectedTeams([value]);
+            }}
+            value={showAllTeams ? "all" : selectedTeams[0]}
+            button={
+              <button
+                className="d-flex align-items-center teams-button"
+                style={{ gap: 3, fontSize: "0.75rem" }}
+              >
+                Teams <ChevronDownIcon style={{ marginTop: "-2" }} />
+              </button>
+            }
+            items={selectItems}
+          />
+          <span>
+            {showAllTeams ? null : (
+              <>
+                {selectedTeams.map((item) => (
+                  <button
+                    onClick={setAllTeamsSelected}
+                    className="d-flex align-items-center"
+                    style={{
+                      gap: ".4em",
+                      fontSize: ".75rem",
+                      padding: ".25em .75em",
+                      border: "1px solid #ccc",
+                      borderRadius: "1em",
+                    }}
+                    key={item}
+                  >
+                    {item}
+                    <Cross2Icon width={10} height={10} />
+                  </button>
+                ))}
+              </>
+            )}
+          </span>
+        </div>
         <span style={{ margin: "auto 0 auto auto", fontSize: ".75rem" }}>
           {count} of {total}
         </span>
@@ -126,6 +180,7 @@ export const StaffMembers = () => {
       <section
         className="staff-members-container"
         style={{
+          overscrollBehavior: "contain",
           height: "500px",
           overflowY: "auto",
           position: "relative",
@@ -153,15 +208,57 @@ export const StaffMembers = () => {
           />
         ))}
         <div
+          className="d-flex justify-content-between align-items-center"
+          key={`${selectedTeams.join("-")}-${count}-${total}`}
           style={{
-            height: 10,
+            margin: "10px 40px",
+            fontSize: ".75rem",
+            animation: `fadeIn .5s ${count * 0.15}s both`,
+          }}
+        >
+          {filteredPeople.length === 0 ? (
+            <div>
+              No matches for &quot;{cleanSearch}&quot;
+              {outsideSearch.length > 0 && selectedTeams.length !== 0
+                ? ` within ${selectedTeams.join(", ")}`
+                : ""}
+            </div>
+          ) : (
+            <div />
+          )}
+          <div>
+            {count} of {total}
+          </div>
+        </div>
+        {outsideSearch.length > 0 && (
+          <>
+            <div
+              style={{
+                padding: "20px 40px",
+                fontSize: ".75rem",
+                borderTop: "1px dotted #ccc",
+              }}
+            >
+              Other results for &quot;{cleanSearch}&quot;
+            </div>
+            {outsideSearch.map((member, i: number) => (
+              // might be nice to add id attached to each member - good for animation
+              <StaffMember
+                itemIndex={i}
+                key={`${member.name}-${i}-${selectedTeams.join("-")}`}
+                onTeamClick={() => setSelectedTeams([member.team])}
+                teamData={teamData}
+                {...member}
+              />
+            ))}
+          </>
+        )}
+        <div
+          style={{
+            height: 0,
             flexShrink: 0,
           }}
         />
-        {/* Text if nothing is selected */}
-        {/* {selectedTeams.length === 1 ? (
-          <button onClick={setAllTeamsSelected}>See all departments</button>
-        ) : null} */}
       </section>
     </div>
   );
